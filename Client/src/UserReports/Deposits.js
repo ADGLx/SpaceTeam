1db1c6094427563c6663ef1af40cee394c7c6f9e
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2023-02-27 15:28:27 -0500

    Just added the functionality of the top right counte
    The counter now counts all the reports

--- Client/src/UserReports/Deposits.js
+++ Client/src/UserReports/Deposits.js
@@ -2,17 +2,40 @@ import * as React from 'react';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Title from './Title';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
 export default function Deposits() {
+  const [reports, setReports] = React.useState(0);
+  //Query the server for all the reports 
+  useEffect(() => {
+    getAllUserReports();
+  }, []);
+
+  function getAllUserReports()
+  {
+        //Get the changes from the server
+        Axios.get('/getUserReports').
+        then(function (response) {
+          //Create a quick new array
+
+           response.data.forEach(element => 
+            {
+              setReports(reports +1);
+           });
+        })
+  }
+
+
   return (
     <React.Fragment>
       <Title>Recent reports</Title>
       <Typography component="p" variant="h4">
-        6
+        {reports}
       </Typography>
       <Typography color="text.secondary" sx={{ flex: 1 }}>
         on 24 February, 2023
