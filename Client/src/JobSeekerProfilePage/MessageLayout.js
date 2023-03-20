102c4c6542962abd3bffb152111cd8e3d3935e75
MuhammadUsman20002 <123197698+MuhammadUsman20002@users.noreply.github.com>
2023-03-19 21:37:37 -0400

    Profile Page & Updating Profile Page
    -Created the profile page for the user
    -Created an update profile page for the user.
    -Allowing the user to access their profile, then modify the profile and in the end save the modification

---
+++ Client/src/JobSeekerProfilePage/MessageLayout.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function MessageLayout() {
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Success!
+      </Typography>
+
+      <Typography variant="h6" gutterBottom>
+       Your Profile has been updated.
+      </Typography>
+      
+    </React.Fragment>
+  );
+}
